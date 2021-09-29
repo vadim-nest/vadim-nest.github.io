6bab302788c9283b1aca23cced47af531076b497
Vadim Gnezdyshkin <vadim@gne.me.uk>
2021-09-29 12:59:12 +0100

    Adding style to buttons

---
+++ cv/CVscript.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    const pdfButton = document.querySelector("#button-pdf");
+    const wordButton = document.querySelector("#button-word");
+    
+    // Buttons on the left styling
+    pdfButton.onmouseover = function() {
+
+    }
+    pdfButton.onmouseout = function() {
+
+
+    }
+
+    wordButton.onmouseover = function() {
+
+    }
+    wordButton.onmouseout = function() {
+    }
+}
