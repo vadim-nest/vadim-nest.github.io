765f64d56a8873b3ba8d1b706f40f0e1fc21b3bc
Vadim Gnezdyshkin <vadim@gne.me.uk>
2021-10-01 11:29:24 +0100

    Adding style to the buttons

--- cv/CVscript.js
+++ cv/CVscript.js
@@ -4,16 +4,23 @@ window.onload = function() {
     
     // Buttons on the left styling
     pdfButton.onmouseover = function() {
-
+        pdfButton.style.transition = "all 0.1s ease-in-out";
+        pdfButton.style.border = "solid 2px beige"
+        pdfButton.style.cursor = "pointer";
     }
     pdfButton.onmouseout = function() {
-
+        pdfButton.style.transition = "all 0.4s ease-in-out";
+        pdfButton.style.border = "solid 2px #ff312e"
 
     }
 
     wordButton.onmouseover = function() {
-
+        wordButton.style.transition = "all 0.1s ease-in-out";
+        wordButton.style.border = "solid 2px beige"
+        wordButton.style.cursor = "pointer";
     }
     wordButton.onmouseout = function() {
+        wordButton.style.transition = "all 0.4s ease-in-out";
+        wordButton.style.border = "solid 2px #ff312e"
     }
 }
