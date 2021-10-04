b4cb47617dc4778fc01004c9aa8575f4f442d670
Vadim Gnezdyshkin <vadim@gne.me.uk>
2021-10-04 13:55:06 +0100

    Styling responsive

--- cv/CVscript.js
+++ cv/CVscript.js
@@ -1,6 +1,10 @@
 window.onload = function() {
     const pdfButton = document.querySelector("#button-pdf");
     const wordButton = document.querySelector("#button-word");
+    const buttonsSize = document.querySelector("#button-pdf").offsetWidth * 2 + 5; // 5 is margin-right for every button (space between buttons)
+    const cvButtons = document.querySelector("#cv-buttons");
+
+
     
     // Buttons on the left styling
     pdfButton.onmouseover = function() {
@@ -42,4 +46,20 @@ window.onload = function() {
     wordButton.addEventListener('mouseup', e => {
         wordButton.style.background = "#ff312e";
     });
+
+
+    buttonsCenterResponsive();
+
+    function buttonsCenterResponsive() {
+        cvButtons.style.left = (window.innerWidth - buttonsSize) / 2 + "px";
+    
+    };
+
+    // You need to finish here (put buttons in place when not mobile screen)
+    window.onresize = function(event) { 
+        buttonsCenterResponsive
+    };
+
 }
+
+
