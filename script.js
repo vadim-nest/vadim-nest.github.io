ef38b49d7c75752e430e9790fd0f4678c95c56aa
Vadim Gnezdyshkin <vadim@gne.me.uk>
2021-10-05 12:38:12 +0100

    Changing spacing on project page

--- script.js
+++ script.js
@@ -547,6 +547,7 @@ window.onload = function () {
 
 
             contactButton.style.fontSize = "1.6em"; 
+            contactButton.style.marginLeft = "20px";
             toggleProjectChanged = true;
 
             contactButton.style.width = "57px";
