1c27b153198aef984f5c34effcccb56d14e49881
Vadim Gnezdyshkin <vadim@gne.me.uk>
2022-12-08 16:29:14 +0000

    Start rebuilding portfolio website. About/Projects

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import '../css/navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav id="navbar">
+      <h2 id='logo' className='h-e-text'>VG</h2>
+      <div id='nav-buttons'>
+        <button className='nav-button'><p className='h-e-text'>About</p></button>
+        <button className='nav-button'><p className='dis-text'>Projects</p></button>
+        <button className='nav-button'><p className='dis-text'>Contact</p></button>
+      </div>
+    </nav>
+  )
+}
