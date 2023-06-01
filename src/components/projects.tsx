abee06af2c2482972db36d4de36559653341e2cf
Vadim Gnezdyshkin <vadim@gne.me.uk>
2023-06-01 14:59:36 +0100

    feat: animate projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,4 +1,10 @@
 import '../css/projects.css';
+import { useState, useRef, TouchEvent } from 'react';
+import 'animate.css';
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Flickity from 'react-flickity-component';
+import 'flickity/css/flickity.css';
+
 // import '~video-react/dist/video-react.css';
 // import videoTest from '../assets/projects-videos/video1809203057.mp4';
 // // Not in use
@@ -18,12 +24,280 @@ import externalLink from '../assets/link.svg';
 import githubLogo from '../assets/github.svg';
 
 export default function Projects() {
+  const flickityRef = useRef<Flickity | null>(null);
+
+  const flickityOptions = {
+    prevNextButtons: false,
+    wrapAround: true,
+  };
+
+
+
+  const flickityOptionsFrontStack = {
+    prevNextButtons: false,
+    wrapAround: true,
+    autoPlay: 1500,
+    freeScroll: true,
+    cellAlign: 'left',
+    pauseAutoPlayOnHover: true,
+    selectedAttraction: 0.001,
+    friction: 1.5,
+  };
+
+  const flickityOptionsBackStack = {
+    prevNextButtons: false,
+    wrapAround: true,
+    autoPlay: 3000,
+    freeScroll: true,
+    cellAlign: 'left',
+    pauseAutoPlayOnHover: true,
+    selectedAttraction: 0.001,
+    friction: 1.5,
+  };
+
+  const [project, setProject] = useState(0);
 
   const arrowNext = (
-    <svg width="15" height="18" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path className="arrow-next-color" d="M-4.93596e-05 9.02381L15.0005 0.364449L14.9994 17.685L-4.93596e-05 9.02381Z" fill="white" fillOpacity="0.38"/>
+    <svg
+      width='15'
+      height='18'
+      viewBox='0 0 15 18'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        className='arrow-next-color'
+        d='M-4.93596e-05 9.02381L15.0005 0.364449L14.9994 17.685L-4.93596e-05 9.02381Z'
+        fill='white'
+        fillOpacity='0.38'
+      />
     </svg>
   );
+
+  const projectVideos = [
+    <img
+      src={tempPic}
+      className='project-video animate__animated animate__slideInRight'
+    ></img>,
+    <img
+      src={tempPic}
+      className='project-video'
+      style={{ filter: 'grayscale(100%)' }}
+    ></img>,
+  ];
+
+  const projects = [
+    <>
+      <h1 className='h-e-text animate__animated animate__fadeInDown'>
+        #newsBuzz
+      </h1>
+      <p className='description animate__animated animate__fadeInDown'>
+        Stay up-to-date on local news by browsing through trending hashtags and
+        clicking on them to read more.
+      </p>
+      <div id='all-stack' className='animate__animated animate__fadeInDown'>
+      <div className='carousel-wrapper'>
+        <div id='front-stack' className='stack'>
+        <Flickity options={flickityOptionsFrontStack}>
+          <div className='bubble bubble-white'>
+            <img className='bubble-img' src={jsLogo} />
+            <p>JavaScript</p>
+          </div>
+          <div className='bubble bubble-black'>
+            <img className='bubble-img' src={reactLogo} />
+            <p>React</p>
+          </div>
+          <div className='bubble bubble-yellow'>
+            <img className='bubble-img' src={cssLogo} />
+            <p>CSS</p>
+          </div>
+          <div className='bubble bubble-white'>
+            <img className='bubble-img' src={jsLogo} />
+            <p>JavaScript</p>
+          </div>
+          <div className='bubble bubble-black'>
+            <img className='bubble-img' src={reactLogo} />
+            <p>React</p>
+          </div>
+          <div className='bubble bubble-yellow'>
+            <img className='bubble-img' src={cssLogo} />
+            <p>CSS</p>
+          </div>
+        </Flickity>
+        </div>
+      </div>
+
+
+        <div id='back-stack' className='stack'>
+        <Flickity options={flickityOptionsBackStack}>
+
+          <div className='bubble bubble-yellow'>
+            <img className='bubble-img' src={jsLogo} />
+            <p>Axios</p>
+          </div>
+          <div className='bubble bubble-white'>
+            <img className='bubble-img' src={reactLogo} />
+            <p>Express.js</p>
+          </div>
+          <div className='bubble bubble-black'>
+            <img className='bubble-img' src={cssLogo} />
+            <p>PostgreSQL</p>
+          </div>
+                    <div className='bubble bubble-yellow'>
+            <img className='bubble-img' src={jsLogo} />
+            <p>Axios</p>
+          </div>
+          <div className='bubble bubble-white'>
+            <img className='bubble-img' src={reactLogo} />
+            <p>Express.js</p>
+          </div>
+          <div className='bubble bubble-black'>
+            <img className='bubble-img' src={cssLogo} />
+            <p>PostgreSQL</p>
+          </div>
+          </Flickity>
+
+        </div>
+
+        <img className='gradient-left' src={gradient} />
+        <img className='gradient-right' src={gradient} />
+      </div>
+      <div className='links  animate__animated animate__fadeInDown'>
+        <img src={externalLink} />
+        <img src={githubLogo} />
+      </div>
+    </>,
+    <>
+      <h1 className='h-e-text'>aMAZEthing</h1>
+      <p className='description'>Description here</p>
+      <div id='all-stack'>
+        <div id='front-stack' className='stack'>
+          <div className='bubble bubble-white'>
+            <img className='bubble-img' src={jsLogo} />
+            <p>yep </p>
+          </div>
+          <div className='bubble bubble-black'>
+            <img className='bubble-img' src={reactLogo} />
+            <p>Reafwqafct</p>
+          </div>
+          <div className='bubble bubble-yellow'>
+            <img className='bubble-img' src={cssLogo} />
+            <p>CqfqwfSS</p>
+          </div>
+        </div>
+
+        <div id='back-stack' className='stack'>
+          <div className='bubble bubble-yellow'>
+            <img className='bubble-img' src={jsLogo} />
+            <p>Axqwfqwfios</p>
+          </div>
+          <div className='bubble bubble-white'>
+            <img className='bubble-img' src={reactLogo} />
+            <p>Expqfqwfress.js</p>
+          </div>
+          <div className='bubble bubble-black'>
+            <img className='bubble-img' src={cssLogo} />
+            <p>PostqwfqwfgreSQL</p>
+          </div>
+        </div>
+
+        <img className='gradient-left' src={gradient} />
+        <img className='gradient-right' src={gradient} />
+      </div>
+      <div className='links'>
+        <img src={externalLink} />
+        <img src={githubLogo} />
+      </div>
+    </>,
+  ];
+
+  function nextProject() {
+    if (project !== projects.length - 1) {
+      setProject(project + 1);
+      flickityRef.current?.next();
+    } else {
+      setProject(0);
+      flickityRef.current?.select(0);
+    }
+  }
+
+  function previousProject() {
+    if (project !== 0) {
+      setProject(project - 1);
+      flickityRef.current?.previous();
+    } else {
+      setProject(projects.length - 1);
+      flickityRef.current?.select(projects.length - 1);
+    }
+  }
+
+  function slideChange() {
+    const changeHandler = (index: number) => {
+      setProject(index);
+    };
+
+    const flkty = flickityRef.current;
+    if (flkty) {
+      flkty.on('change', changeHandler);
+    }
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      if (flkty) {
+        flkty.off('change', changeHandler);
+      }
+    };
+  }
+
+  const touchStartRef = useRef<{ x: number; y: number } | null>(null);
+  const [swipeHandled, setSwipeHandled] = useState(false);
+
+  const handleTouchStart = (event: TouchEvent<HTMLDivElement>) => {
+    touchStartRef.current = {
+      x: event.touches[0].clientX,
+      y: event.touches[0].clientY,
+    };
+    setSwipeHandled(false);
+  };
+
+  const handleTouchMove = (event: TouchEvent<HTMLDivElement>) => {
+    if (!touchStartRef.current || swipeHandled) {
+      return;
+    }
+
+    const touchEnd = {
+      x: event.touches[0].clientX,
+      y: event.touches[0].clientY,
+    };
+
+    const horizontalDistance = touchEnd.x - touchStartRef.current.x;
+    const verticalDistance = touchEnd.y - touchStartRef.current.y;
+
+    // Calculate the absolute distance in each direction
+    const absHorizontalDistance = Math.abs(horizontalDistance);
+    const absVerticalDistance = Math.abs(verticalDistance);
+
+    // Check if the vertical distance is greater than the horizontal distance
+    if (absVerticalDistance > absHorizontalDistance) {
+      // It's a scroll, not a swipe
+      return;
+    }
+
+    const swipeDirection = horizontalDistance > 0 ? 'right' : 'left';
+
+    // Handle the swipe direction
+    if (swipeDirection === 'right') {
+      nextProject();
+    } else {
+      previousProject();
+    }
+
+    setSwipeHandled(true);
+  };
+
+  const handleTouchEnd = () => {
+    touchStartRef.current = null;
+    setSwipeHandled(false);
+  };
 
   return (
     <div id='projects-part'>
@@ -33,60 +307,34 @@ export default function Projects() {
           <source src={videoTest}
                   type="video/mp4"/>
         </video> */}
-        <img src={tempPic} className='project-video'></img>
+        <Flickity
+          flickityRef={(el) => {
+            flickityRef.current = el;
+            slideChange();
+          }}
+          options={flickityOptions}
+        >
+          {projectVideos}
+        </Flickity>
       </div>
-      <div className='project-description'>
-        <h1 className='h-e-text'>#newsBuzz</h1>
-        <p className='description'>
-          Stay up-to-date on local news by browsing through trending hashtags
-          and clicking on them to read more.
-        </p>
-        <div id='all-stack'>
-          <div id='front-stack' className='stack'>
-            <div className='bubble bubble-white'>
-              <img className='bubble-img' src={jsLogo} />
-              <p>JavaScript</p>
-            </div>
-            <div className='bubble bubble-black'>
-              <img className='bubble-img' src={reactLogo} />
-              <p>React</p>
-            </div>
-            <div className='bubble bubble-yellow'>
-              <img className='bubble-img' src={cssLogo} />
-              <p>CSS</p>
-            </div>
-
-          </div>
-
-          <div id='back-stack' className='stack'>
-            <div className='bubble bubble-yellow'>
-              <img className='bubble-img' src={jsLogo} />
-              <p>Axios</p>
-            </div>
-            <div className='bubble bubble-white'>
-              <img className='bubble-img' src={reactLogo} />
-              <p>Express.js</p>
-            </div>
-            <div className='bubble bubble-black'>
-              <img className='bubble-img' src={cssLogo} />
-              <p>PostgreSQL</p>
-            </div>
-          </div>
-
-          <img className='gradient-left' src={gradient}/>
-          <img className='gradient-right' src={gradient}/>
-
-        </div>
-        <div className='links'>
-          <img src={externalLink} />
-          <img src={githubLogo} />
-        </div>
+      <div
+        className='project-description'
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {projects[project]}
       </div>
 
       <div id='arrows'>
-        <div className='arrow-left'>{arrowNext}</div>
-        <div className='arrow-right'>{arrowNext}</div>
+        <div className='arrow-left' onClick={previousProject}>
+          {arrowNext}
+        </div>
+        <div className='arrow-right' onClick={nextProject}>
+          {arrowNext}
+        </div>
       </div>
+      <div className='control-container'></div>
     </div>
   );
 }
--- src/components/theArrow.jsx
+++ src/components/theArrow.jsx
@@ -1,125 +1,80 @@
 import '../css/theArrow.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function TheArrow() {
-
-
-
-
-  // function getAngle(offset) {
-  //   const delta = 1; // A small value to calculate the tangent
-  //   const point1 = thePath.getPointAtLength(offset);
-  //   const point2 = thePath.getPointAtLength(offset + delta);
-  //   const dx = point2.x - point1.x;
-  //   const dy = point2.y - point1.y;
-  //   const angle = Math.atan2(dy, dx) * (180 / Math.PI);
-  //   return angle;
-  // }
-
-  // function updateArrowPosition(dashoffset) {
-  //   const offset = thePath.getTotalLength() - dashoffset;
-  //   const arrowPosition = thePath.getPointAtLength(offset);
-  //   let angle;
-
-  //   // If the dashoffset is between 210 and 810, set the angle to 90 degrees
-  //   angle = getAngle(offset);
-
-  //   if (dashoffset > 300 && dashoffset < 810) angle = 0;
-  //   if (dashoffset < 20) angle = 180;
-
-  //   const arrowHead = document.getElementById('arrowHead');
-
-  //   // Adjust these values to align the arrowhead with the path
-  //   let arrowHeadOffsetX = 1.5;
-  //   const arrowHeadOffsetY = -3;
-
-  //   if (dashoffset < 10) {
-  //     arrowHeadOffsetX += 10; // Adjust this value to shift the arrow to the right
-  //   } else {
-  //     arrowHeadOffsetX = 1.5;
-  //   }
-  //   console.log(arrowPosition.x + arrowHeadOffsetX, arrowPosition.y + arrowHeadOffsetY);
-    // arrowHead.setAttribute(
-    //   'transform',
-    //   `translate(${arrowPosition.x + arrowHeadOffsetX},${
-    //     arrowPosition.y + arrowHeadOffsetY
-    //   }) rotate(${angle})`
-    // );
-  // }
+  const pathRef = useRef(null);
+  const maskRef = useRef(null);
+  const wrapRef = useRef(null);
+  const arrowHeadRef = useRef(null);
 
   useEffect(() => {
-    const thePath = document.getElementById('thePath');
-    const mask = document.getElementById('mask');
 
-    const l = thePath.getTotalLength();
-    const dasharray = l;
-    mask.style.strokeDasharray = dasharray;
-    let dashoffset = l - 1600;
-    mask.style.strokeDashoffset = dashoffset;
+    const handleScroll = () => {
+      if (pathRef.current && maskRef.current && wrapRef.current) {
+        const thePath = pathRef.current;
+        const mask = maskRef.current;
+        const wrap = wrapRef.current;
 
-    // updateArrowPosition(dashoffset);
+        const l = thePath.getTotalLength();
+        const dasharray = l;
+        mask.style.strokeDasharray = dasharray;
+        mask.style.strokeDashoffset = dasharray;
 
-    const lengthAbout = 1620;
-    const lengthProjects = 410;
+        const totalScrollableHeight = wrap.scrollHeight - wrap.clientHeight;
+        let dashoffset = l - (window.scrollY * l) / totalScrollableHeight;
 
-    ///
-    window.addEventListener('scroll', function () {
-      const wrap = document.getElementById('wrap');
-      const totalScrollableHeight = wrap.scrollHeight - wrap.clientHeight;
-      dashoffset = l - (window.scrollY * l) / totalScrollableHeight;
+        // update dashoffset based on different conditions
+        const lengthAbout = 1620;
+        const lengthProjects = 410;
 
-      if (dashoffset > lengthAbout) dashoffset = lengthAbout;
-      if (dashoffset < lengthAbout && dashoffset > 810) dashoffset = dashoffset;
-      if (dashoffset > 210 && dashoffset < 810) dashoffset = lengthProjects;
-      if (dashoffset < 210) dashoffset = dashoffset + 200;
+        let arrowRightElement = document.querySelector('.arrow-right .arrow-next-color');
+        let lightBigElement = document.querySelector('.light-big');
+        let lightSmallElement = document.querySelector('.light-small');
 
-      // dashoffset === lengthAbout ? arrowHead.style.visibility = 'visible' : arrowHead.style.visibility = 'hidden';
+        if (dashoffset > lengthAbout) dashoffset = lengthAbout;
+        else if (dashoffset > 210 && dashoffset < 810) dashoffset = lengthProjects;
+        else if (dashoffset < 210) dashoffset += 200;
 
-      if (dashoffset === lengthAbout) {
-        document.getElementById('arrowHead').style.opacity = '1';
-      } else {
-        document.getElementById('arrowHead').style.opacity = '0';
+        if (arrowRightElement) {
+          if (dashoffset === lengthProjects) {
+            arrowRightElement.style.fill = 'var(--yellow-main)';
+            arrowRightElement.style.fillOpacity = '1';
+          } else {
+            arrowRightElement.style.fill = 'var(--light-gray)';
+          }
+        }
+
+        if (lightBigElement && lightSmallElement) {
+          if (dashoffset < 10) {
+            lightBigElement.style.visibility = 'visible';
+            lightSmallElement.style.visibility = 'visible';
+          } else {
+            lightBigElement.style.visibility = 'hidden';
+            lightSmallElement.style.visibility = 'hidden';
+          }
+        }
+
+        if (arrowHeadRef.current) {
+          arrowHeadRef.current.style.opacity = dashoffset === lengthAbout ? '1' : '0';
+        }
+
+        mask.style.strokeDashoffset = dashoffset;
       }
+    };
 
-      // requestAnimationFrame(() => {
-      //   if (dashoffset === lengthAbout) {
-      //     document.getElementById('arrowHead').style.visibility = 'visible';
-      //   } else {
-      //     document.getElementById('arrowHead').style.visibility = 'hidden';
-      //   }
-      // });
+    handleScroll();
 
-      document.getElementById('arrowHead').setAttribute(
-        'transform',
-        `translate(254, 713) rotate(90)`
-      );
+    window.addEventListener('scroll', handleScroll);
 
-
-      if (dashoffset === lengthProjects) {
-        document.querySelector('.arrow-right .arrow-next-color').style.fill = 'var(--yellow-main)';
-        document.querySelector('.arrow-right .arrow-next-color').style.fillOpacity = '1';
-      } else {
-        document.querySelector('.arrow-right .arrow-next-color').style.fill = 'var(--light-gray)';
-      }
-
-      if (dashoffset < 10) {
-        document.querySelector('.light-big').style.visibility = 'visible';
-        document.querySelector('.light-small').style.visibility = 'visible';
-      } else {
-        document.querySelector('.light-big').style.visibility = 'hidden';
-        document.querySelector('.light-small').style.visibility = 'hidden';
-      }
-
-
-      mask.style.strokeDashoffset = dashoffset;
-      console.log(dashoffset);
-      // updateArrowPosition(dashoffset);
-    });
+    // Clean up event listener
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
-    <div id='wrap'>
-      <svg
+    <div id='wrap' ref={wrapRef}>
+    <svg
         id='svg'
         viewBox='0 0 370 1900'
         fill='none'
@@ -127,10 +82,12 @@ export default function TheArrow() {
       >
         <defs>
           <mask id='mask1'>
-            <use id='mask' xlinkHref='#thePath' />
+          <use ref={maskRef} xlinkHref='#thePath' />
           </mask>
           <path
             id='thePath'
+            ref={pathRef}
+
             d='M164.876 1C171.043 44.1667 143.139 77.3269 70.6388 108C-33.3612 152 9.13878 264 18.6388 353.5C24.4115 407.886 47.2145 492.854 132.814 534.054C239.814 585.554 319.217 442.842 244.639 343C200.901 284.446 45.9382 328.5 59.4382 474.5C70.2382 591.3 163.472 640.833 204.639 655C220.139 662 249.676 668.1 252.876 716.5C256.876 777 266.713 891.554 192.876
           932.5C172.139 944 38.8763 992 38.8763 1141.5C38.8763 1261.1 38.8763 1455
           38.8763
@@ -149,11 +106,14 @@ export default function TheArrow() {
         />
         <path
           id='arrowHead'
+          ref={arrowHeadRef}
+
           className='cool arrowHead'
           d='M0,-15L0,-12L23,0L0,12L0,15'
           // visibility='hidden'
+          transform='translate(254, 713) rotate(90)'
         />
-        </svg>
+      </svg>
     </div>
   );
 }
