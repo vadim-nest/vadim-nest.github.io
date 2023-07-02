f934a6b7ecd981b72d56bbfd40ae1bf59b1000ab
Vadim Gnezdyshkin <vadim@gne.me.uk>
2023-07-02 17:54:31 +0100

    Updates

--- assets/index.9dc3cc50.js
+++ assets/index.9dc3cc50.js
@@ -1,4 +1,4 @@
-import{r as H}from"./index.9b12a9bc.js";function dt(m,a){for(var h=0;h<a.length;h++){const f=a[h];if(typeof f!="string"&&!Array.isArray(f)){for(const l in f)if(l!=="default"&&!(l in m)){const r=Object.getOwnPropertyDescriptor(f,l);r&&Object.defineProperty(m,l,r.get?r:{enumerable:!0,get:()=>f[l]})}}}return Object.freeze(Object.defineProperty(m,Symbol.toStringTag,{value:"Module"}))}var X={exports:{}},A={exports:{}},L={exports:{}};/*!
+import{r as H}from"./index.b7dc29ed.js";function dt(m,a){for(var h=0;h<a.length;h++){const f=a[h];if(typeof f!="string"&&!Array.isArray(f)){for(const l in f)if(l!=="default"&&!(l in m)){const r=Object.getOwnPropertyDescriptor(f,l);r&&Object.defineProperty(m,l,r.get?r:{enumerable:!0,get:()=>f[l]})}}}return Object.freeze(Object.defineProperty(m,Symbol.toStringTag,{value:"Module"}))}var X={exports:{}},A={exports:{}},L={exports:{}};/*!
  * getSize v2.0.3
  * measure size of elements
  * MIT license
